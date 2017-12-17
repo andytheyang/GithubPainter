8d5f26b309a57afb24585bbbea3cdc8c6832d0ec
Andy Yang <andytheyang@gmail.com>
2017-12-16 21:01:10 -0800

    custom config parameters

--- painter.sh
+++ painter.sh
@@ -2,6 +2,19 @@
 
 config_file="config.json"
 
+if [ "$#" -eq 1 ]; then
+    config_file=$1
+    echo "using $config_file"
+elif [[ "$#" -gt 1 ]]; then
+    echo "too many arguments"
+    exit 1  
+fi
+
+if [ ! -f "$config_file" ]; then
+    echo "$config_file does not exist"
+    exit 1
+fi
+
 repodir="$( jq -r '.repositorydir' "$config_file" )"
 factor="$( jq -r '.commitfactor' "$config_file" )"
 
